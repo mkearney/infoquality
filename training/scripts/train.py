5a902a79767222acabf1066051b3af7b864e5423
kearneymw <mike.wayne.kearney@gmail.com>
2023-08-22 07:50:51 -0400

    edit train script

--- training/scripts/train.py
+++ training/scripts/train.py
@@ -59,16 +59,17 @@ def parse_args() -> Namespace:
 
 
 def main(args: Namespace):
-    print("__start__")
-
     # -------------------------------------------------------------------
-    # GET DATASETS
+    # DATASETS: INFO QUALLITY
     # -------------------------------------------------------------------
     if False:
         train_df = pl.read_parquet("/Users/mwk/data/info-quality-train.parquet")
         valid_df = pl.read_parquet("/Users/mwk/data/info-quality-valid.parquet")
         test_df = pl.read_parquet("/Users/mwk/data/info-quality-test.parquet")
         label_map = {"low": 0, "high": 1}
+    # -------------------------------------------------------------------
+    # DATASETS: IMDB SENTIMENT
+    # -------------------------------------------------------------------
     else:
         imdb = load_dataset("imdb")
         train_df = pl.DataFrame(
@@ -95,8 +96,10 @@ def main(args: Namespace):
         test_df = valid_df[splitrow:, :]
         valid_df = valid_df[:splitrow, :]
         label_map = {"neg": 0, "pos": 1}
-    print(f"Size of train: {train_df.shape[0]:,}")
-    print(f"Size of valid: {valid_df.shape[0]:,}")
+    print("-" * 76)
+    print(f"         train nobs:  {train_df.shape[0]:,}")
+    print(f"         valid nobs:  {valid_df.shape[0]:,}")
+    print(f"          test nobs:  {test_df.shape[0]:,}")
     train_data = MessagesDataset(
         messages=train_df["text"].to_list(),
         labels=train_df["label"].to_list(),
@@ -114,9 +117,16 @@ def main(args: Namespace):
     )
 
     # -------------------------------------------------------------------
+    # HYPERPARAMETERS
+    # -------------------------------------------------------------------
+    print("-" * 76)
+    hp = get_hyperparameters_from_args(args)
+    for k, v in hp.__dict__.items():
+        print(f" {k:>18}:  {v}")
+
+    # -------------------------------------------------------------------
     # HYPERPARAMETERS & MODEL COMPONENTS
     # -------------------------------------------------------------------
-    hp = get_hyperparameters_from_args(args)
     preprocessor = Preprocessor(max_len=hp.max_len)
     embeddings = bert_embeddings[:, : hp.embedding_dim].detach().clone()  # type: ignore
     model = Model(
@@ -146,18 +156,16 @@ def main(args: Namespace):
     best_metric, best_state_dict = 0, model.state_dict()
     valid_loss, acc, f1s = 0, [], []
     total_loss = 0
-
+    start_time = datetime.now()
     for epoch in range(hp.num_epochs):
         if epoch == 0:
-            now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            print(f"{now}")
-            print("-" * 73)
+            print("-" * 76)
             print(
-                f"{'time':>19} {'ep':>2} {'lr0e1':>7} {'lr1e1':>7}  {'loss':>6}  "
+                f" {'time':>19}  {'ep':>2} {'lr0e1':>7} {'lr1e1':>7}  {'loss':>6}  "
                 f" {'val':>6}   {'acc':>6}   {'f1':>6}"
             )
-            print("-" * 73)
-        total_loss = 0
+            print("-" * 76)
+        train_loss = []
         model.train()
         lr0 = optimizer.param_groups[0]["lr"]
         lr1 = lr0
@@ -169,14 +177,14 @@ def main(args: Namespace):
             optimizer.step()
             lr_scheduler.step()
             lr1 = optimizer.param_groups[0]["lr"]
-            total_loss += loss.item() / len(targets)
+            train_loss.append(loss.item() / len(targets))
             if i == hp.num_steps:
                 break
 
         # Validation
         model.eval()
         with torch.no_grad():
-            valid_loss, acc, f1s = 0, [], []
+            valid_loss, acc, f1s = [], [], []
             for i, (vmessages, vtargets) in enumerate(valid_dataloader):
                 voutputs = model(vmessages)
                 vloss = criterion(voutputs, vtargets)
@@ -190,47 +198,34 @@ def main(args: Namespace):
                     best_metric = epoch_f1
                     best_state_dict = model.state_dict()
 
-                valid_loss += vloss.item() / len(vtargets)
+                valid_loss.append(vloss.item() / len(vtargets))
                 if i == hp.num_steps:
                     break
 
         now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        tlss = sum(train_loss) / len(train_loss)
         vacc = sum(acc) / len(acc)
         vf1 = sum(f1s) / len(f1s)
+        vlss = sum(valid_loss) / len(valid_loss)
         print(
-            f"{now} {epoch:2d} {lr0:6.5f} {lr1:6.5f}  {total_loss:6.4f}  "
-            f" {valid_loss:6.4f}   {vacc:4.4f}   {vf1:4.4f}"
+            f" {now}  {epoch:2d} {lr0:6.5f} {lr1:6.5f}  {tlss:6.4f}  "
+            f" {vlss:6.4f}   {vacc:4.4f}   {vf1:4.4f}"
         )
         if epoch == hp.num_epochs - 1:
-            print("-" * 73)
-            now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            print(f"{now}")
-
-    # -------------------------------------------------------------------
-    # SAVE MODEL
-    # -------------------------------------------------------------------
-    model.load_state_dict(best_state_dict)
-    model.eval()
-    metrics = {
-        "loss": total_loss,
-        "f1": f1s,
-        "acc": acc,
-        "val": valid_loss,
-    }
-    metrics_path = saver.save_metrics(metrics)
-    print(f"Model metrics saved as {metrics_path}")
-    embeddings_path = saver.save_embeddings(model)
-    print(f"Model saved as {embeddings_path}")
-    state_dict_path = saver.save_state_dict(model)
-    print(f"Model state dict saved as {state_dict_path}")
-    hyperparameters_path = saver.save_hyperparameters(model)
-    print(f"Model hyperparameters saved as {hyperparameters_path}")
-
+            print("-" * 76)
+            end_time = datetime.now()
+            duration = end_time - start_time
+            if duration.total_seconds() <= 120:
+                print(f"     train_duration:  {duration.total_seconds():,.2f} seconds")
+            else:
+                print(
+                    f"     train_duration:  {duration.total_seconds()/60:,.2f} minutes"
+                )
     # -------------------------------------------------------------------
     # TEST
     # -------------------------------------------------------------------
     with torch.no_grad():
-        test_loss, acc, f1s = 0, [], []
+        test_loss, acc, f1s = [], [], []
         for i, (tmessages, ttargets) in enumerate(test_dataloader):
             toutputs = model(tmessages)
             tloss = criterion(toutputs, ttargets)
@@ -240,13 +235,38 @@ def main(args: Namespace):
             )
             epoch_f1 = f1(toutputs, ttargets)
             f1s.append(epoch_f1)
-            test_loss += tloss.item() / len(ttargets)
+            test_loss.append(tloss.item() / len(ttargets))
             if i == hp.num_steps:
                 break
-    test_acc = sum(acc) / len(acc)
-    test_f1 = sum(f1s) / len(f1s)
-    print(f"Test accuracy: {test_acc*100:.2f}%")
-    print(f"Test F1: {test_f1*100:.2f}%")
+        print("-" * 76)
+        tacc = sum(acc) / len(acc)
+        tf1 = sum(f1s) / len(f1s)
+        tlss = sum(test_loss) / len(test_loss)
+        print(f"          test_loss:  {tlss:.4f}")
+        print(f"           test_acc:  {tacc:.2f}")
+        print(f"            test_f1:  {tf1:.4f}")
+
+    # -------------------------------------------------------------------
+    # SAVE MODEL
+    # -------------------------------------------------------------------
+    print("-" * 76)
+    model.load_state_dict(best_state_dict)
+    model.eval()
+    metrics = {
+        "loss": total_loss,
+        "f1": f1s,
+        "acc": acc,
+        "val": valid_loss,
+    }
+    metrics_path = saver.save_metrics(metrics)
+    print(f"            metrics:  {metrics_path}")
+    embeddings_path = saver.save_embeddings(model)
+    print(f"         embeddings:  {embeddings_path}")
+    state_dict_path = saver.save_state_dict(model)
+    print(f"         state_dict:  {state_dict_path}")
+    hyperparameters_path = saver.save_hyperparameters(model)
+    print(f"    hyperparameters:  {hyperparameters_path}")
+    print("-" * 76)
 
 
 if __name__ == "__main__":
